8bc6d273b21526147d4199b20f97a2c6b607a287
Tejumola <o.a.tejumola@gmail.com>
2022-09-09 05:55:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#!/bin/bash
-#include <stdio.h>
-
+echo include <stdio.h>
 /**
  * main - Entry point
- *
+ * description: prints "\"Programming is like building a multilingual puzzle"*
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
-}
+	return (0);
+}' > 4-puts.c
