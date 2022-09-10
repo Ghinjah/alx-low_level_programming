b4dc0cc8e0d1e1c52207cd6b5590e7474edfca6d
Tejumola <o.a.tejumola@gmail.com>
2022-09-10 04:02:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
  * main-program entry point.
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59\);
+	write(2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1)
 		;
 }
