121bdf7b19de19cae9ba3759f1040a5517fd727e
Tejumola <o.a.tejumola@gmail.com>
2022-09-12 03:50:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main-program entry point.
  *
- * Return: 1 on success.
+ * Return 1 on sucess.
  */
 int main(void)
 {
-	write("and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59)
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1)
 		;
 }
