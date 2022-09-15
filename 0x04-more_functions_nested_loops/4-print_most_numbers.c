5b5dc87abd5bcdfb9cd5f7f6a1794ffa32200d2a
Tejumola <o.a.tejumola@gmail.com>
2022-09-15 13:44:03 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,11 @@ void print_most_numbers(void)
 {
 	int n;
 	
-	for (n = 48; n < 58; n++);
+	for (n = 48; n < 58; n++)
 	{
-		if ((n == 50) || (n == 52));
+		if ((n == 50) || (n == 52))
 		{
-			continue;
+			continue
 		}
 		putchar(n);
 	}
